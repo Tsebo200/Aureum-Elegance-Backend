8aa3880cb48cd499e1bf1a191fbb4eeb63a9b192
TshwetsoMo <221411@virtualwindow.co.za>
2025-03-25 17:07:47 +0200

    Ingredients components

---
+++ Mystefy/Controllers/IngredientsController.cs
@@ -0,0 +1,53 @@
+using System.Threading.Tasks;
+using Mystefy.Models;
+using Mystefy.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Mystefy.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class IngredientsController : ControllerBase
+    {
+        private readonly IIngredientRepository _service;
+
+        public IngredientsController(IIngredientRepository service)
+        {
+            _service = service;
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult<Ingredients>> CreateIngredient([FromBody] Ingredients ingredient)
+        {
+            Ingredients newIngredient = await _service.CreateIngredientAsync(ingredient);
+            return Ok(newIngredient);
+        }
+
+        [HttpGet("{ingredientId}")]
+        public async Task<ActionResult<Ingredients>> GetIngredientWithRecipes(int ingredientId)
+        {
+            Ingredients? ingredient = await _service.GetIngredientWithRecipesAsync(ingredientId);
+            if (ingredient == null)
+            {
+                return NotFound("Ingredient not found");
+            }
+            return Ok(ingredient);
+        }
+
+        [HttpPost("addtorecipe")]
+        public async Task<ActionResult> AddIngredientToRecipe([FromQuery] int ingredientId, [FromQuery] int recipeId)
+        {
+            await _service.AddIngredientToRecipeAsync(ingredientId, recipeId);
+            return Ok("Ingredient added to recipe");
+        }
+
+        [HttpPost("removefromrecipe")]
+        public async Task<ActionResult> RemoveIngredientFromRecipe([FromQuery] int ingredientId, [FromQuery] int recipeId)
+        {
+            await _service.RemoveIngredientFromRecipeAsync(ingredientId, recipeId);
+            return Ok("Ingredient removed from recipe");
+        }
+    }
+}
+
+
---
+++ Mystefy/Interfaces/IIngredientRepository.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Mystefy.Models;
+
+namespace Mystefy.Interfaces
+{
+    public interface IIngredientRepository
+    {
+        Task<Ingredients> CreateIngredientAsync(Ingredients ingredient);
+        Task<Ingredients?> GetIngredientWithDetailsAsync(int ingredientId);
+        Task<Ingredients?> GetIngredientWithRecipesAsync(int ingredientId);
+        Task AddIngredientToRecipeAsync(int ingredientId, int recipeId);
+        Task<Ingredients?> GetIngredientByNameAsync(string ingredientName);
+        Task<Ingredients?> GetIngredientByTypeAsync(string ingredientType);
+        Task<Ingredients?> GetIngredientByCostAsync(string ingredientCost);
+        Task<Ingredients?> GetIngredientByIsExpiredAsync(bool isExpired);
+        Task<Ingredients?> UpdateIngredientAsync(Ingredients ingredient);
+        Task<Ingredients?> DeleteIngredientAsync(int ingredientId);
+        Task RemoveIngredientFromRecipeAsync(int ingredientId, int recipeId);
+    }
+}
+
