405d5ae0a8b8607c3ccb9e7e71b523f41a1d6ace
Sean-D-231121 <231121@virtualwindow.co.za>
2025-03-25 12:31:10 +0200

    update Dbsets

--- Mystefy/Data/MystefyDbContext.cs
+++ Mystefy/Data/MystefyDbContext.cs
@@ -9,4 +9,7 @@ public class MystefyDbContext : DbContext
     public MystefyDbContext(DbContextOptions<MystefyDbContext> options) : base(options) {}
     public DbSet<Packaging> Packaging { get; set; }
     public DbSet<Warehouse> Warehouses { get; set; }
+    public DbSet<WarehouseStock> WarehouseStocksarehouseStocks { get; set; }
+    public DbSet<Fragrance> Fragrances { get; set; }
+    public DbSet<FragranceIngredient> FragranceIngredients { get; set; }
 }
