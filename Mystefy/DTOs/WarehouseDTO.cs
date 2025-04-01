61dee4680ab68d44465bff3c89e0f808e006fc7c
Sean-D-231121 <231121@virtualwindow.co.za>
2025-04-01 11:17:08 +0200

    WarehouseDTOMade

---
+++ Mystefy/DTOs/WarehouseDTO.cs
@@ -0,0 +1,9 @@
+using System;
+
+namespace Mystefy.DTOs;
+
+public class WarehouseDTO
+{
+    public string Name {get; set;} = string.Empty;
+    public string location {get; set;} = string.Empty;
+}
