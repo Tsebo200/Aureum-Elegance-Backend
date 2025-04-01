15fef0a5dc21292efbc0b19c3c5fc3e6315c8a96
Sean-D-231121 <231121@virtualwindow.co.za>
2025-04-01 17:24:19 +0200

    Added WarehouseStockService and FragranceService

---
+++ Mystefy/Interfaces/IFragranceService.cs
@@ -0,0 +1,13 @@
+using System;
+using Mystefy.Models;
+
+namespace Mystefy.Interfaces;
+
+public interface IFragranceService
+{
+    Task<IEnumerable<Fragrance>> GetAllFragrances();
+        Task<Fragrance?> GetFragranceById(int id);
+        Task<Fragrance> AddFragrance(Fragrance fragrance);
+        Task<bool> UpdateFragrance(int id, Fragrance fragrance);
+        Task<bool> DeleteFragrance(int id);
+}
