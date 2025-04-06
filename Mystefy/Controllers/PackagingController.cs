79f37719119b4e7e2c2645227e10225b3527fc36
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-04-06 12:25:08 +0200

    Feat(DTO Packaging) Update Packaging Controller
    Packing Controller has update that display the PackagingData the manner in which the Packaging DTO is displayed, this can now be seen in SwaggerUI (REST API Visualiser not an Entity Framework)

--- Mystefy/Controllers/PackagingController.cs
+++ Mystefy/Controllers/PackagingController.cs
@@ -6,44 +6,115 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Mystefy.Data;
+using Mystefy.DTOs;
 using Mystefy.Models;
 
 namespace Mystefy.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class packagingController : ControllerBase
+    public class PackagingController : ControllerBase
     {
         private readonly MystefyDbContext _context;
 
-        public packagingController(MystefyDbContext context)
+        public PackagingController(MystefyDbContext context)
         {
             _context = context;
         }
 
-        // GET: api/packaging
+        // GET: api/Packaging
         [HttpGet]
-        public async Task<ActionResult<IEnumerable<Packaging>>> GetPackaging()
+        public async Task<ActionResult<IEnumerable<PackagingDTO>>> GetPackaging()
         {
-            return await _context.Packaging.ToListAsync();
+            var packagingList = await _context.Packaging
+                .Include(p => p.FinishedProduct)
+                .Select(p => new PackagingDTO
+                {
+                    Id = p.Id,
+                    Name = p.Name,
+                    Type = p.Type,
+                    Stock = p.Stock,
+                    FinishedProduct = p.FinishedProduct.Select(fp => new PackagingFinishedProductDTO
+                    {
+                        ProductID = fp.ProductID,
+                        FragranceID = fp.FragranceID,
+                        PackagingID = fp.PackagingID,
+                        Quantity = fp.Quantity
+                    }).FirstOrDefault()
+                })
+                .ToListAsync();
+
+            return Ok(packagingList);
         }
 
-        // GET: api/packaging/5
+        // GET: api/Packaging/5
         [HttpGet("{id}")]
-        public async Task<ActionResult<Packaging>> GetPackaging(int id)
+        public async Task<ActionResult<PackagingDTO>> GetPackaging(int id)
         {
-            var packaging = await _context.Packaging.FindAsync(id);
+            var packaging = await _context.Packaging
+                .Include(p => p.FinishedProduct)
+                .FirstOrDefaultAsync(p => p.Id == id);
 
             if (packaging == null)
             {
                 return NotFound();
             }
 
-            return packaging;
+            var packagingDTO = new PackagingDTO
+            {
+                Id = packaging.Id,
+                Name = packaging.Name,
+                Type = packaging.Type,
+                Stock = packaging.Stock,
+                FinishedProduct = packaging.FinishedProduct.Select(fp => new PackagingFinishedProductDTO
+                {
+                    ProductID = fp.ProductID,
+                    FragranceID = fp.FragranceID,
+                    PackagingID = fp.PackagingID,
+                    Quantity = fp.Quantity
+                }).FirstOrDefault()
+            };
+
+            return Ok(packagingDTO);
         }
 
-        // PUT: api/packaging/5
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        // POST: api/Packaging
+        [HttpPost]
+        public async Task<ActionResult<PackagingDTO>> PostPackaging(Packaging packaging)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            _context.Packaging.Add(packaging);
+            await _context.SaveChangesAsync();
+
+            // Load related entities
+            await _context.Entry(packaging)
+                .Collection(p => p.FinishedProduct)
+                .LoadAsync();
+
+            // Convert to DTO
+            var packagingDTO = new PackagingDTO
+            {
+                Id = packaging.Id,
+                Name = packaging.Name,
+                Type = packaging.Type,
+                Stock = packaging.Stock,
+                FinishedProduct = packaging.FinishedProduct.Select(fp => new PackagingFinishedProductDTO
+                {
+                    ProductID = fp.ProductID,
+                    FragranceID = fp.FragranceID,
+                    PackagingID = fp.PackagingID,
+                    Quantity = fp.Quantity
+                }).FirstOrDefault()
+            };
+
+            return CreatedAtAction(nameof(GetPackaging), new { id = packaging.Id }, packagingDTO);
+        }
+
+        // PUT: api/Packaging/5
         [HttpPut("{id}")]
         public async Task<IActionResult> PutPackaging(int id, Packaging packaging)
         {
@@ -57,6 +128,28 @@ namespace Mystefy.Controllers
             try
             {
                 await _context.SaveChangesAsync();
+
+                // Load related entities after update
+                await _context.Entry(packaging)
+                    .Collection(p => p.FinishedProduct)
+                    .LoadAsync();
+
+                var packagingDTO = new PackagingDTO
+                {
+                    Id = packaging.Id,
+                    Name = packaging.Name,
+                    Type = packaging.Type,
+                    Stock = packaging.Stock,
+                    FinishedProduct = packaging.FinishedProduct.Select(fp => new PackagingFinishedProductDTO
+                    {
+                        ProductID = fp.ProductID,
+                        FragranceID = fp.FragranceID,
+                        PackagingID = fp.PackagingID,
+                        Quantity = fp.Quantity
+                    }).FirstOrDefault()
+                };
+
+                return Ok(packagingDTO);
             }
             catch (DbUpdateConcurrencyException)
             {
@@ -69,22 +162,9 @@ namespace Mystefy.Controllers
                     throw;
                 }
             }
-
-            return NoContent();
         }
 
-        // POST: api/packaging
-        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
-        [HttpPost]
-        public async Task<ActionResult<Packaging>> PostPackaging(Packaging packaging)
-        {
-            _context.Packaging.Add(packaging);
-            await _context.SaveChangesAsync();
-
-            return CreatedAtAction("GetPackaging", new { id = packaging.Id }, packaging);
-        }
-
-        // DELETE: api/packaging/5
+        // DELETE: api/Packaging/5
         [HttpDelete("{id}")]
         public async Task<IActionResult> DeletePackaging(int id)
         {
