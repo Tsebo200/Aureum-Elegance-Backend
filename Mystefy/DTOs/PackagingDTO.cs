902228494991b8ed449202f7d7182f4f2755b7b5
Tsebo Ramonyalioa <200200@virtualwindow.co.za>
2025-04-06 11:24:37 +0200

    Feat(DTO) Update Packaging DTO complete
    Packaging DTO update now incorporates Finished Products data. Now must update Packaging Controller with DTO

--- Mystefy/DTOs/PackagingDTO.cs
+++ Mystefy/DTOs/PackagingDTO.cs
@@ -4,10 +4,18 @@ namespace Mystefy.DTOs;
 
 public class PackagingDTO
 {
-
+    public int Id{ get; set; }
     public required string Name{ get; set; }
     public string? Type{ get; set; }
     public int Stock{ get; set; }
+    public PackagingFinishedProductDTO? FinishedProduct { get; set; }
 
 }
 
+public class PackagingFinishedProductDTO
+{
+        public int ProductID { get; set; }
+        public int FragranceID { get; set; }
+        public int PackagingID { get; set; }
+        public int Quantity { get; set; }
+}
